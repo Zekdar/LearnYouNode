82261aff56f2680de98dbd01e1803af0aae12ef9
zekdar <julien.hairapian@gmail.com>
2016-05-22 10:44:10 +0200

    add package.json

--- ex06_main.js
+++ ex06_main.js
@@ -1,4 +1,4 @@
-var filterDir = require('./ex6_module');
+var filterDir = require('./ex06_module');
 
 var dirPath = process.argv[2];
 var extension = process.argv[3];
@@ -7,6 +7,6 @@ filterDir(dirPath, extension, function(err, data) {
 	if(!err) {
 		data.forEach(function(item) {
 			console.log(item);
-		});		
+		});
 	}
 });
